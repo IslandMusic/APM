a7aa7a52632f3bb8760efdd60c2c893841d4f1fe
nwnolte <nwnolte@gmail.com>
2016-12-15 22:45:46 -0800

    Clean build + Contribute

--- app/src/main/java/islandmusic/apm/StartApp.java
+++ app/src/main/java/islandmusic/apm/StartApp.java
@@ -14,7 +14,7 @@ public class StartApp extends AppCompatActivity {
         setContentView(R.layout.activity_start_app);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainScreenActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
