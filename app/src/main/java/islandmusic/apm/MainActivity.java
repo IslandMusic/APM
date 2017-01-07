9356674c89417c969333e3cede3376ace02c82b9
nwnolte <nwnolte@gmail.com>
2017-01-06 23:42:49 -0800

    Adding AWS Cognito credentials and user data

--- app/src/main/java/islandmusic/apm/MainActivity.java
+++ app/src/main/java/islandmusic/apm/MainActivity.java
@@ -24,4 +24,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void ConnectAWS (){
+
+        // http://docs.aws.amazon.com/mobile/sdkforandroid/developerguide/setup.html
+        // Get AWS credentials
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-west-2:d18d3245-afb8-4bba-b010-d38eef72d706", // Identity Pool ID
+                Regions.US_WEST_2 // Region
+        );
+
+        // Store User Data ~DO WE NEED THIS?~
+        // Initialize the Cognito Sync client
+        CognitoSyncManager syncClient = new CognitoSyncManager(
+                getApplicationContext(),
+                Regions.US_WEST_2, // Region
+                credentialsProvider);
+
+        // Create a record in a dataset and synchronize with the server
+        Dataset dataset = syncClient.openOrCreateDataset("myDataset");
+        dataset.put("myKey", "myValue");
+        dataset.synchronize(new DefaultSyncCallback() {
+            @Override
+            public void onSuccess(Dataset dataset, List newRecords) {
+                //Your handler code here
+            }
+        });
+    }
+
 }
