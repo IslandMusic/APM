bad6b82ab1991bfc0ed93e3ab39ad2f6788606ba
nwnolte <nwnolte@gmail.com>
2017-04-01 22:50:43 -0700

    lineariy

--- app/src/main/java/islandmusic/apm/Contribute.java
+++ app/src/main/java/islandmusic/apm/Contribute.java
@@ -1,7 +1,9 @@
 package islandmusic.apm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contribute extends AppCompatActivity {
 
@@ -10,11 +12,12 @@ public class Contribute extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contribute);
     }
+
+    /** Called when the user clicks the Looking button*/
+    public void launchPicture(View view) {
+        Intent intent = new Intent(this, ContPhotoActivity.class);
+        startActivity(intent);
+
+    }
 }
 
-/** Called when the user clicks the Looking button*/
- public void launchPicture(View view) {
- Intent intent = new Intent(this, ContPhotoActivity.class);
- startActivity(intent);
-
- }
